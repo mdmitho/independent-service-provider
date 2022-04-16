982cd800645b7df747c8cedab7fe0b5da32cf25d
Hossen <mdmitho199911@gmail.com>
2022-04-17 00:40:02 +0600

    slider done

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import slider1 from '../../../src/img/slider/slider1.png'
+import slider2 from '../../../src/img/slider/slider2.png'
+import slider3 from '../../../src/img/slider/slider3.png'
+
+import './Slider.css'
 
 const Slider = () => {
     return (
-        <div>
-            <h1>this is slider</h1>
+        <div className=''>
+          
+
+            <Carousel >
+  <Carousel.Item interval={1000}>
+      <div className="slider">
+      <img
+      className="d-block w-100 "
+      src={slider1}
+      alt="First slide"
+    />
+      </div>
+  
+    <Carousel.Caption>
+      <h3>DENTIST CARE</h3>
+      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={500}>
+      <div className="slider">
+      <img
+      className="d-block w-100 "
+      src={slider2}
+      alt="Third slide"
+    />
+      </div>
+   
+    <Carousel.Caption>
+      <h3>DENTIST CARE</h3>
+      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+      <div className="slider">
+      <img
+      className="d-block w-100 "
+      src={slider3}
+      alt="Third slide"
+    />
+      </div>
+  
+    <Carousel.Caption>
+      <h3>DENTIST CARE</h3>
+      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
         </div>
     );
 };
