df8aae9858457b7fef7373d0de89d10050927517
Hossen <mdmitho199911@gmail.com>
2022-06-19 13:59:34 +0600

    slider chance

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -8,54 +8,49 @@ import './Slider.css'
 
 const Slider = () => {
     return (
-        <div className=''>
-          
+      <div className="h-75">
+        <Carousel className="">
+          <Carousel.Item interval={2000}>
+            <div className="slider">
+              <img className=" w-100 " src={slider1} alt="First slide" />
+            </div>
 
-            <Carousel >
-  <Carousel.Item interval={2000}>
-      <div className="slider">
-      <img
-      className="d-block w-100 "
-      src={slider1}
-      alt="First slide"
-    />
+            <Carousel.Caption>
+              <h3>DENTIST CARE</h3>
+              <p>
+                All of our locations offer comprehensive services, with compassionate care, at
+                transparent pricing and need.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={1000}>
+            <div className="slider">
+              <img className=" w-100 " src={slider2} alt="Third slide" />
+            </div>
+
+            <Carousel.Caption>
+              <h3>DENTIST CARE</h3>
+              <p>
+                All of our locations offer comprehensive services, with compassionate care, at
+                transparent pricing and need.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <div className="slider">
+              <img className=" w-100 " src={slider3} alt="Third slide" />
+            </div>
+
+            <Carousel.Caption>
+              <h3>DENTIST CARE</h3>
+              <p>
+                All of our locations offer comprehensive services, with compassionate care, at
+                transparent pricing and need.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
-  
-    <Carousel.Caption>
-      <h3>DENTIST CARE</h3>
-      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item interval={1000}>
-      <div className="slider">
-      <img
-      className="d-block w-100 "
-      src={slider2}
-      alt="Third slide"
-    />
-      </div>
-   
-    <Carousel.Caption>
-      <h3>DENTIST CARE</h3>
-      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-      <div className="slider">
-      <img
-      className="d-block w-100 "
-      src={slider3}
-      alt="Third slide"
-    />
-      </div>
-  
-    <Carousel.Caption>
-      <h3>DENTIST CARE</h3>
-      <p>All of our locations offer comprehensive services, with compassionate care, at transparent pricing and need.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-        </div>
     );
 };
 
